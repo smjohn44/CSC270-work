e824940dbd5747493c761a51d8a9b66892d13931
smjohn44 <sjohnson6@furman.edu>
2019-12-13 12:31:44 -0500

    Updated web writer

--- src/main/scala/web-writer.scala
+++ src/main/scala/web-writer.scala
@@ -196,7 +196,7 @@ object webWriter {
 			<link href="https://fonts.googleapis.com/css?family=EB+Garamond:400,400i,500,500i,600,600i,700,700i,800,800i&amp;subset=cyrillic-ext,greek,greek-ext,latin-ext" rel="stylesheet">
 			</head>
 			<body>
-			<header>Your header</header>
+			<header>Table of Contents</header>
 			<article>
 			${sequenceString}
 			${catString}
@@ -206,7 +206,7 @@ object webWriter {
 			${corpString}
 			${navString}
 			</article>
-			<footer>Your footer</footer>
+			<footer><a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>.</footer>
 			</body>
 			</html>
 			"""
@@ -254,7 +254,7 @@ object webWriter {
 
 		val titleString: String = workCat.toString // can fancify this
 
-		val infoString: String = """<div class="cts_siteDesc">Whatever you want to say here.</div>"""
+		val infoString: String = """<div class="cts_siteDesc">Why are the changes I'm making not showing?</div>"""
 
 		val tocHeader: String = """<div class="cts_toc">Table of Contents</div>"""
 
